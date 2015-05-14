99b489f08a6a4df675492d06eb48e774883a10b3
Cory Flanagan <cflan12@gmail.com>
2015-05-13 21:38:40 -0700

    added angular ui.utils for query filter on app as bower component

--- employee-time-tracker/public/scripts/app.js
+++ employee-time-tracker/public/scripts/app.js
@@ -7,6 +7,7 @@
 	angular
 		.module('timeTracker', [
 			'ngResource',
-			'ui.bootstrap'
+			'ui.bootstrap',
+			'ui.utils'
 			]);
 })();
--- employee-time-tracker/resources/views/index.php
+++ employee-time-tracker/resources/views/index.php
@@ -179,6 +179,7 @@
     <script type="text/javascript" src="bower_components/angular-bootstrap/ui-bootstrap-tpls.js"></script>
     <script type="text/javascript" src="bower_components/angular-resource/angular-resource.js"></script>
     <script type="text/javascript" src="bower_components/moment/moment.js"></script>
+    <script type="text/javascript" src="bower_components/angular-ui-utils/ui-utils.js"></script>
 
     <!-- Application Scripts -->
     <script type="text/javascript" src="scripts/app.js"></script>
